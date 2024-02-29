69e6c6b96d5a06f900114506af846cb2e77609a0
Björn Hermansson <bjorn@brorsligt.com>
2024-02-29 20:48:55 +0100

    Update styling

--- pages/index.vue
+++ pages/index.vue
@@ -4,44 +4,58 @@
     <Meta name="description" content="asdasd" />
     <!-- <Meta name="og:image" content="" /> -->
   </Head>
-  <div class="grid gap-4 p-4">
+  <div class="grid gap-4 px-6">
 
-    <div class="w-12 md:w-20">
+    <div class="w-12 md:w-20 fixed top-0 left-6">
       <div class="aspect-square bg-pink-500 p-4">
         <!-- <AtomIcon class="w-4" name="fanny-logo" /> -->
       </div>
     </div>
 
-    <div class="box p-1">
-      <h1 class="font-bold text-xl">Fanny Buhler</h1>
-      <p class="text-sm">A communicative and design driven frontend developer currently working at Made People.</p>
-    </div>
+    <!-- Content -->
+    <div class="fixed left-0 bottom-[20%] w-full px-6">
+      <!-- Intro -->
+      <div class="grid gap-2 mb-8 md:mb-24 md:w-full md:max-w-[50vw] mx-auto">
+        <h1 class="font-medium text-xl md:text-5xl">Fanny Buhler</h1>
+        <p class="text-sm md:text-lg max-w-lg">A communicative and design driven frontend developer currently working at Made People.</p>
+      </div>
 
-    <div class="box p-1 text-xs md:text-sm">
-      <div class="box p-1">Recent Projects</div>
-      <div class="box p-1">
-        <a class="block box p-1" href="https://minirodini.com" target="_blank">
-          <div class="">Mini Rodini</div>
-          <div class="">https://minirodini.com</div>
-        </a>
-        <a class="block box p-1" href="https://astridlindgren.com" target="_blank">
-          <div class="">Astrid Lindgren</div>
-          <div class="">https://astridlindgren.com</div>
-        </a>
-        <a class="block box p-1" href="https://rohnisch.com" target="_blank">
-          <div class="">Röhnisch</div>
-          <div class="">https://rohnisch.com</div>
-        </a>
-        <a class="block box p-1" href="https://oascompany.com" target="_blank">
-          <div class="">OAS</div>
-          <div class="">https://oascompany.com</div>
-        </a>
-        <a class="block box p-1" href="https://eqpestore.com" target="_blank">
-          <div class="">Skistar EQPE</div>
-          <div class="">https://eqpestore.com</div>
-        </a>
+      <!-- Projects -->
+      <div class="-mr-6 flex justify-end">
+        <div class="text-xs md:text-lg w-full md:max-w-[55vw]">
+          <div class="mb-3">Recent Projects</div>
+          <div class="border-b border-b-black">
+            <a class="block flex items-center border-t border-t-black py-3 pr-4" href="https://minirodini.com" target="_blank">
+              <div class="w-2/5">Mini Rodini</div>
+              <div class="flex-1">https://minirodini.com</div>
+              <AtomIcon class="w-4" name="arrow-right" />
+            </a>
+            <a class="block flex items-center border-t border-t-black py-3 pr-4" href="https://astridlindgren.com" target="_blank">
+              <div class="w-2/5">Astrid Lindgren</div>
+              <div class="flex-1">https://astridlindgren.com</div>
+              <AtomIcon class="w-4" name="arrow-right" />
+            </a>
+            <a class="block flex items-center border-t border-t-black py-3 pr-4" href="https://rohnisch.com" target="_blank">
+              <div class="w-2/5">Röhnisch</div>
+              <div class="flex-1">https://rohnisch.com</div>
+              <AtomIcon class="w-4" name="arrow-right" />
+            </a>
+            <a class="block flex items-center border-t border-t-black py-3 pr-4" href="https://oascompany.com" target="_blank">
+              <div class="w-2/5">OAS</div>
+              <div class="flex-1">https://oascompany.com</div>
+              <AtomIcon class="w-4" name="arrow-right" />
+            </a>
+            <a class="block flex items-center border-t border-t-black py-3 pr-4" href="https://eqpestore.com" target="_blank">
+              <div class="w-2/5">Skistar EQPE</div>
+              <div class="flex-1">https://eqpestore.com</div>
+              <AtomIcon class="w-4" name="arrow-right" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
+
+    <!-- Footer -->
     <div class="fixed bottom-0 left-0 w-full px-6 md:px-12 py-4 md:py-6">
       <div class="text-center flex items-center justify-center md:justify-between text-xs md:text-sm">
         <div class="hidden md:block">© 2024</div>
