98c3539d2e150e0eb3e3a422af8dc86fe2ace585
Björn Hermansson <bjorn@brorsligt.com>
2024-02-29 20:55:55 +0100

    Update logo

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
     <!-- <Meta name="og:image" content="" /> -->
   </Head>
   <div class="grid gap-4 px-6">
-    <div class="w-12 md:w-20 fixed top-0 left-6">
+    <div class="w-12 md:w-20 fixed top-0 left-6 md:left-12">
       <div class="aspect-square bg-pink-500 p-4">
         <!-- <AtomIcon class="w-4" name="fanny-logo" /> -->
       </div>
