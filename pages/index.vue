8b649f2e37d96e9d087ad8a6537912ad4566050c
Fanny <fanny.buhler@madepeople.se>
2024-02-29 20:51:12 +0100

    favivon

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,16 @@
   <Head>
     <Title>Fansan</Title>
     <Meta name="description" content="asdasd" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff0090" />
+    <meta name="msapplication-TileColor" content="#ff0090" />
+    <meta name="theme-color" content="#ffffff" />
     <!-- <Meta name="og:image" content="" /> -->
   </Head>
   <div class="grid gap-4 p-4">
-
     <div class="w-12 md:w-20">
       <div class="aspect-square bg-pink-500 p-4">
         <!-- <AtomIcon class="w-4" name="fanny-logo" /> -->
@@ -14,7 +20,10 @@
 
     <div class="box p-1">
       <h1 class="font-bold text-xl">Fanny Buhler</h1>
-      <p class="text-sm">A communicative and design driven frontend developer currently working at Made People.</p>
+      <p class="text-sm">
+        A communicative and design driven frontend developer currently working
+        at Made People.
+      </p>
     </div>
 
     <div class="box p-1 text-xs md:text-sm">
@@ -24,7 +33,11 @@
           <div class="">Mini Rodini</div>
           <div class="">https://minirodini.com</div>
         </a>
-        <a class="block box p-1" href="https://astridlindgren.com" target="_blank">
+        <a
+          class="block box p-1"
+          href="https://astridlindgren.com"
+          target="_blank"
+        >
           <div class="">Astrid Lindgren</div>
           <div class="">https://astridlindgren.com</div>
         </a>
@@ -43,19 +56,31 @@
       </div>
     </div>
     <div class="fixed bottom-0 left-0 w-full px-6 md:px-12 py-4 md:py-6">
-      <div class="text-center flex items-center justify-center md:justify-between text-xs md:text-sm">
+      <div
+        class="text-center flex items-center justify-center md:justify-between text-xs md:text-sm"
+      >
         <div class="hidden md:block">© 2024</div>
         <div class="flex items-center gap-4">
-          <a class="block" href="mailto:fanny.buhler1@gmail.com">
-            [ email ]
-          </a>
-          <a class="block" href="https://www.linkedin.com/in/fanny-buhler" target="_blank">
+          <a class="block" href="mailto:fanny.buhler1@gmail.com"> [ email ] </a>
+          <a
+            class="block"
+            href="https://www.linkedin.com/in/fanny-buhler"
+            target="_blank"
+          >
             [ linkedin ]
           </a>
-          <a class="block" href="https://open.spotify.com/user/1132908801?si=fd6b115c7caa468f" target="_blank">
+          <a
+            class="block"
+            href="https://open.spotify.com/user/1132908801?si=fd6b115c7caa468f"
+            target="_blank"
+          >
             [ spotify ]
           </a>
-          <a class="block" href="https://www.instagram.com/fannyvafan" target="_blank">
+          <a
+            class="block"
+            href="https://www.instagram.com/fannyvafan"
+            target="_blank"
+          >
             [ instagram ]
           </a>
         </div>
@@ -65,5 +90,5 @@
 </template>
 
 <script setup>
-// 
+//
 </script>
