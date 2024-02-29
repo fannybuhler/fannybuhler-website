23a2389c2c7d70d3288896ddde8903dbeec0fe3a
Björn Hermansson <bjorn@brorsligt.com>
2024-02-29 20:30:42 +0100

    Update footer styling

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
       <p class="text-sm">A communicative and design driven frontend developer currently working at Made People.</p>
     </div>
 
-    <div class="box p-1">
+    <div class="box p-1 text-xs md:text-sm">
       <div class="box p-1">Recent Projects</div>
       <div class="box p-1">
         <a class="block box p-1" href="https://minirodini.com" target="_blank">
@@ -42,22 +42,23 @@
         </a>
       </div>
     </div>
-
-    <div class="box p-1 text-center flex items-center justify-between">
-      <div class="hidden md:block">© 2024</div>
-      <div class="flex items-center gap-4">
-        <a class="block" href="mailto:fanny.buhler1@gmail.com">
-          [ email ]
-        </a>
-        <a class="block" href="https://www.linkedin.com/in/fanny-buhler" target="_blank">
-          [ linkedin ]
-        </a>
-        <a class="block" href="https://open.spotify.com/user/1132908801?si=fd6b115c7caa468f" target="_blank">
-          [ spotify ]
-        </a>
-        <a class="block" href="https://www.instagram.com/fannyvafan" target="_blank">
-          [ instagram ]
-        </a>
+    <div class="fixed bottom-0 left-0 w-full px-6 md:px-12 py-4 md:py-6">
+      <div class="text-center flex items-center justify-center md:justify-between text-xs md:text-sm">
+        <div class="hidden md:block">© 2024</div>
+        <div class="flex items-center gap-4">
+          <a class="block" href="mailto:fanny.buhler1@gmail.com">
+            [ email ]
+          </a>
+          <a class="block" href="https://www.linkedin.com/in/fanny-buhler" target="_blank">
+            [ linkedin ]
+          </a>
+          <a class="block" href="https://open.spotify.com/user/1132908801?si=fd6b115c7caa468f" target="_blank">
+            [ spotify ]
+          </a>
+          <a class="block" href="https://www.instagram.com/fannyvafan" target="_blank">
+            [ instagram ]
+          </a>
+        </div>
       </div>
     </div>
   </div>
