359cf4045150630a8592f13580659bc0698795da
Fanny <fanny.buhler@madepeople.se>
2024-02-29 22:24:46 +0100

    duration

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,23 +5,28 @@
     >
       <div class="hidden md:block">© 2024</div>
       <div class="flex items-center gap-4 md:gap-12">
-        <a class="transition hover:text-[#FF0090]" href="mailto:fanny.buhler1@gmail.com"> [ email ] </a>
         <a
-          class="transition hover:text-[#FF0090]"
+          class="transition duration-300 hover:text-[#FF0090]"
+          href="mailto:fanny.buhler1@gmail.com"
+        >
+          [ email ]
+        </a>
+        <a
+          class="transition duration-300 hover:text-[#FF0090]"
           href="https://www.linkedin.com/in/fanny-buhler"
           target="_blank"
         >
           [ linkedin ]
         </a>
         <a
-          class="transition hover:text-[#FF0090]"
+          class="transition duration-300 hover:text-[#FF0090]"
           href="https://open.spotify.com/user/1132908801?si=fd6b115c7caa468f"
           target="_blank"
         >
           [ spotify ]
         </a>
         <a
-          class="transition hover:text-[#FF0090]"
+          class="transition duration-300 hover:text-[#FF0090]"
           href="https://www.instagram.com/fannyvafan"
           target="_blank"
         >
@@ -33,5 +38,5 @@
 </template>
 
 <script async setup>
-// 
+//
 </script>
