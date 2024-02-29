33c14d9851cfeaa1aa21cd2c9af852998d9dc786
Fanny <fanny.buhler@madepeople.se>
2024-02-29 21:03:35 +0100

    projects

--- pages/index.vue
+++ pages/index.vue
@@ -16,60 +16,7 @@
 
     <div class="fixed left-0 bottom-[20%] w-full px-6">
       <Intro />
-
-      <!-- Projects -->
-      <div class="-mr-6 flex justify-end">
-        <div class="text-xs md:text-lg w-full md:max-w-[55vw]">
-          <div class="mb-3">Recent Projects</div>
-          <div class="border-b border-b-black">
-            <a
-              class="block flex items-center border-t border-t-black py-3 pr-4"
-              href="https://minirodini.com"
-              target="_blank"
-            >
-              <div class="w-2/5">Mini Rodini</div>
-              <div class="flex-1">https://minirodini.com</div>
-              <AtomIcon class="w-4" name="arrow-right" />
-            </a>
-            <a
-              class="block flex items-center border-t border-t-black py-3 pr-4"
-              href="https://astridlindgren.com"
-              target="_blank"
-            >
-              <div class="w-2/5">Astrid Lindgren</div>
-              <div class="flex-1">https://astridlindgren.com</div>
-              <AtomIcon class="w-4" name="arrow-right" />
-            </a>
-            <a
-              class="block flex items-center border-t border-t-black py-3 pr-4"
-              href="https://rohnisch.com"
-              target="_blank"
-            >
-              <div class="w-2/5">Röhnisch</div>
-              <div class="flex-1">https://rohnisch.com</div>
-              <AtomIcon class="w-4" name="arrow-right" />
-            </a>
-            <a
-              class="block flex items-center border-t border-t-black py-3 pr-4"
-              href="https://oascompany.com"
-              target="_blank"
-            >
-              <div class="w-2/5">OAS</div>
-              <div class="flex-1">https://oascompany.com</div>
-              <AtomIcon class="w-4" name="arrow-right" />
-            </a>
-            <a
-              class="block flex items-center border-t border-t-black py-3 pr-4"
-              href="https://eqpestore.com"
-              target="_blank"
-            >
-              <div class="w-2/5">Skistar EQPE</div>
-              <div class="flex-1">https://eqpestore.com</div>
-              <AtomIcon class="w-4" name="arrow-right" />
-            </a>
-          </div>
-        </div>
-      </div>
+      <Projects />
     </div>
 
     <Footer />
