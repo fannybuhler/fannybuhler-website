1ed25f63f7b8d52e0042cebdf26b62fe7a884a90
Fanny <fanny.buhler@madepeople.se>
2024-03-01 08:45:44 +0100

    Fix animation

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid gap-2 mb-8 md:mb-24 md:w-full md:max-w-[50vw] mx-auto">
+  <div class="grid gap-2 mb-14 md:mb-24 md:w-full md:max-w-[50vw] mx-auto">
     <h1 class="font-medium text-xl md:text-5xl">Fanny Buhlér</h1>
     <p class="text-sm md:text-lg max-w-lg">
       A communicative and design driven frontend developer currently working at
--- components/Projects.vue
+++ components/Projects.vue
@@ -5,102 +5,77 @@
       <div @mouseleave="active = 0">
         <a
           class="flex items-center border-y py-3 pr-3 md:pr-6 group transition duration-500"
-          :class="
-            active === 0
-              ? 'text-black border-black border-b-transparent'
-              : active === 1
-              ? 'text-black border-black border-b-black'
-              : 'text-black/20 border-t-black/20 border-b-transparent'
-          "
+          :class="active === 0 ? inActiveStyling : activeStyling(1, active)"
           href="https://minirodini.com"
           target="_blank"
           @mouseover="active = 1"
+          @click="active = 0"
         >
           <div class="w-[35%] md:w-2/5">Mini Rodini</div>
           <div class="flex-1">www.minirodini.com</div>
           <AtomIcon
             name="arrow-right"
-            class="w-4 group-hover:scale-125 origin-left group-hover:text-hotpink transition duration-300"
+            class="w-4 md:group-hover:scale-125 origin-left md:group-hover:text-hotpink transition duration-300 text-hotpink md:text-black"
           />
         </a>
         <a
           class="flex items-center border-y py-3 pr-3 md:pr-6 group transition duration-500"
-          :class="
-            active === 0
-              ? 'text-black border-black border-b-transparent'
-              : active === 2
-              ? 'text-black border-black border-b-black'
-              : 'text-black/20 border-t-black/20 border-b-transparent'
-          "
+          :class="active === 0 ? inActiveStyling : activeStyling(2, active)"
           href="https://astridlindgren.com"
           target="_blank"
           @mouseover="active = 2"
+          @click="active = 0"
         >
           <div class="w-[35%] md:w-2/5">Astrid Lindgren</div>
           <div class="flex-1">www.astridlindgren.com</div>
           <AtomIcon
             name="arrow-right"
-            class="w-4 group-hover:scale-125 origin-left group-hover:text-hotpink transition duration-300"
+            class="w-4 md:group-hover:scale-125 origin-left md:group-hover:text-hotpink transition duration-300 text-hotpink md:text-black"
           />
         </a>
         <a
           class="flex items-center border-y py-3 pr-3 md:pr-6 group transition duration-500"
-          :class="
-            active === 0
-              ? 'text-black border-black border-b-transparent'
-              : active === 3
-              ? 'text-black border-black border-b-black'
-              : 'text-black/20 border-t-black/20 border-b-transparent'
-          "
+          :class="active === 0 ? inActiveStyling : activeStyling(3, active)"
           href="https://rohnisch.com"
           target="_blank"
           @mouseover="active = 3"
+          @click="active = 0"
         >
           <div class="w-[35%] md:w-2/5">Röhnisch</div>
           <div class="flex-1">www.rohnisch.com</div>
           <AtomIcon
             name="arrow-right"
-            class="w-4 group-hover:scale-125 origin-left group-hover:text-hotpink transition duration-300"
+            class="w-4 md:group-hover:scale-125 origin-left md:group-hover:text-hotpink transition duration-300 text-hotpink md:text-black"
           />
         </a>
         <a
           class="flex items-center border-y py-3 pr-3 md:pr-6 group transition duration-500"
-          :class="
-            active === 0
-              ? 'text-black border-black border-b-transparent'
-              : active === 4
-              ? 'text-black border-black border-b-black'
-              : 'text-black/20 border-t-black/20 border-b-transparent'
-          "
+          :class="active === 0 ? inActiveStyling : activeStyling(4, active)"
           href="https://oascompany.com"
           target="_blank"
           @mouseover="active = 4"
+          @click="active = 0"
         >
           <div class="w-[35%] md:w-2/5">OAS</div>
           <div class="flex-1">www.oascompany.com</div>
           <AtomIcon
             name="arrow-right"
-            class="w-4 group-hover:scale-125 origin-left group-hover:text-hotpink transition duration-300"
+            class="w-4 md:group-hover:scale-125 origin-left md:group-hover:text-hotpink transition duration-300 text-hotpink md:text-black"
           />
         </a>
         <a
           class="flex items-center border-y py-3 pr-3 md:pr-6 group transition duration-500"
-          :class="
-            active === 0
-              ? 'text-black border-black border-b-transparent'
-              : active === 5
-              ? 'text-black border-black border-b-black'
-              : 'text-black/20 border-t-black/20 border-b-transparent'
-          "
+          :class="active === 0 ? inActiveStyling : activeStyling(5, active)"
           href="https://eqpestore.com"
           target="_blank"
           @mouseover="active = 5"
+          @click="active = 0"
         >
           <div class="w-[35%] md:w-2/5">Skistar EQPE</div>
           <div class="flex-1">www.eqpestore.com</div>
           <AtomIcon
             name="arrow-right"
-            class="w-4 group-hover:scale-125 origin-left group-hover:text-hotpink transition duration-300"
+            class="w-4 md:group-hover:scale-125 origin-left md:group-hover:text-hotpink transition duration-300 text-hotpink md:text-black"
           />
         </a>
       </div>
@@ -110,4 +85,15 @@
 
 <script async setup>
 const active = ref(0);
+const activeStyling = (index, active) => {
+  if (index === active) {
+    return "text-black border-black border-b-black";
+  }
+  if (index === active + 1) {
+    return "text-black/20 border-b-transparent border-t-transparent";
+  }
+  return "text-black/20 border-t-black/20 border-b-transparent";
+};
+const inActiveStyling =
+  "text-black border-t-black border-b-transparent last:border-b-black";
 </script>
